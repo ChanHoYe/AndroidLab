070231258871afc1db0387168b44662521ee8fec
ChanHoYe <koreannet200@gmail.com>
2018-04-09 16:20:21 +0900

    Modify Lab2-2

--- Lab2/Lab2-2/app/src/main/java/com/ch/lab2/MainActivity.java
+++ Lab2/Lab2-2/app/src/main/java/com/ch/lab2/MainActivity.java
@@ -3,11 +3,9 @@ package com.ch.lab2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText editUrl;
@@ -16,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Connect View Variables and Views and Set onClickListener.
         editUrl = (EditText)findViewById(R.id.editUrl);
         Button btnNext = (Button)findViewById(R.id.btnNext);
         btnNext.setOnClickListener(this);
@@ -27,15 +26,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnNext:
                 String url = editUrl.getText().toString();
 
-                if(!TextUtils.isEmpty(url)) {
-                    // Start new activity. (intent contains URL value)
-                    Intent intent = new Intent(MainActivity.this, NewActivity.class);
-                    intent.putExtra("url", url);
-                    startActivity(intent);
-                } else {
-                    // If URL value is empty, Showing toast message instead of starting new activity.
-                    Toast.makeText(this, "값을 입력해 주십시오.", Toast.LENGTH_SHORT).show();
-                }
+                // Start new activity. (intent contains URL value)
+                Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                intent.putExtra("url", url);
+                startActivity(intent);
+
                 break;
         }
     }
--- Lab2/Lab2-2/app/src/main/java/com/ch/lab2/NewActivity.java
+++ Lab2/Lab2-2/app/src/main/java/com/ch/lab2/NewActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewActivity extends AppCompatActivity implements View.OnClickListener {
     String url;
@@ -32,7 +34,9 @@ public class NewActivity extends AppCompatActivity implements View.OnClickListen
             textUrl.setText(url);
 
             // If URL value not contains 'http://', append 'http://' to URL value.
-            if(!url.contains("http")) {
+            if(TextUtils.isEmpty(url))
+                Toast.makeText(this, "주소를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+            else if(!url.contains("http")) {
                 StringBuffer buf = new StringBuffer();
                 buf.append("http://");
                 buf.append(url);
@@ -46,9 +50,11 @@ public class NewActivity extends AppCompatActivity implements View.OnClickListen
         switch(v.getId()) {
             case R.id.btnGo:
                 // Start activity with URL(Web View).
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                if(!TextUtils.isEmpty(url))
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
                 break;
             case R.id.btnBack:
+                Toast.makeText(this, "뒤로가기 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
                 finish();
                 break;
         }
